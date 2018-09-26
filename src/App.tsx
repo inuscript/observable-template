9b33a0d5e070a223f0eecf1c08971632812c8613
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-09-26 15:54:18 +0900

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import * as React from "react"
 import { generateStore, simpleConnect } from "./store/store"
 import { Provider } from "react-redux"
+//@ts-ignore
+import { createDevTools } from "redux-devtools"
+//@ts-ignore
+import LogMonitor from "redux-devtools-log-monitor"
 
+const Devtool = createDevTools(<LogMonitor />)
 const store = generateStore()
 
 const MainInner = (props) => {
@@ -15,6 +20,7 @@ class App extends React.Component {
   public render() {
     return (
       <Provider store={store}>
+        <Devtool />
         <Main />
       </Provider>
     )
