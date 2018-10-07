fbcb2cd859e4dec2dbe0037bdfc0307f3be757a3
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-10-07 21:35:33 +0900

    add

--- src/epic/konamiCommandEpic.ts
+++ src/epic/konamiCommandEpic.ts
@@ -1,8 +1,8 @@
-import { fromEvent, Observable, of, concat } from "rxjs"
+import { fromEvent, Observable, concat } from "rxjs"
 
 import { takeUntilHotReload } from "./hotReload"
 
-import { pluck, scan, map, filter, tap, mergeMap } from "rxjs/operators"
+import { pluck, scan, map, filter } from "rxjs/operators"
 
 const command = ["↑", "↑", "↓", "↓", "←", "→", "←", "→", "B", "A"]
 
@@ -23,15 +23,6 @@ const convertVisible = (key) => {
   return false
 }
 
-const scanValidKeymap = () => (stream$): Observable<string[]> =>
-  stream$.pipe(
-    pluck("key"),
-    filter(convertVisible),
-    map(convertVisible),
-    scan<string>((curr, next) => [...curr, next], []),
-    map((cmd: string[]) => cmd.slice(-1 * command.length))
-  )
-
 const detectSuccess = (latestCmd: string[]) => ({
   type: "KONAMI_COMMAND",
   payload: command.join("") === latestCmd.join("")
@@ -46,10 +37,13 @@ const loggingEpic = (source$: Observable<string[]>) =>
   source$.pipe(map(logging))
 
 export const konamiCommandEpic = () => {
-  return fromEvent(document, "keydown").pipe(
+  const stream$ = fromEvent(document, "keydown").pipe(
     takeUntilHotReload(),
-    scanValidKeymap(),
-    mergeMap((cmd) => concat(of(logging(cmd)), of(detectSuccess(cmd))))
-    // map((a) => console.log(a))
+    pluck("key"),
+    filter(convertVisible),
+    map(convertVisible),
+    scan<string>((curr, next) => [...curr, next], []),
+    map((cmd: string[]) => cmd.slice(-1 * command.length))
   )
+  return concat(stream$.pipe(map(detectSuccess)), stream$.pipe(map(logging)))
 }
