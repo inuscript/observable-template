dcd0ac70e97062e2337804565379c8da866cc4d5
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-10-16 21:18:42 +0900

    add

--- src/epic/pingEpic.ts
+++ src/epic/pingEpic.ts
@@ -1,5 +1,6 @@
 import { ofType, combineEpics } from "redux-observable"
-import { ignoreElements, tap, map } from "rxjs/operators"
+import { ignoreElements, tap, map, mergeMap } from "rxjs/operators"
+import { merge, concat, of } from "rxjs";
 
 const debug = () => (source) =>
   source.pipe(
@@ -7,33 +8,69 @@ const debug = () => (source) =>
     ignoreElements()
   )
 
-// ボタンからPINGのActionが来たら中間のTEMP_PINGを発火
-const seedEpic = (action$) =>
-  action$.pipe(
-    ofType("PING"),
-    map((action: any) => ({
-      type: "TEMP_PING",
-      payload: action.payload
-    }))
-  )
+// // ボタンからPINGのActionが来たら中間のTEMP_PINGを発火
+// const seedEpic = (action$) =>
+//   action$.pipe(
+//     ofType("PING"),
+//     map((action: any) => ({
+//       type: "TEMP_PING",
+//       payload: action.payload
+//     }))
+//   )
 
-//
-const firstEpic = (action$) =>
-  action$.pipe(
-    ofType("TEMP_PING"),
+// //
+// const doubleEpic = (action$) =>
+//   action$.pipe(
+//     ofType("TEMP_PING"),
+//     map((action: any) => ({
+//       type: "PONG",
+//       payload: action.payload * 3
+//     }))
+//   )
+
+// const tripleEpic = (action$) =>
+//   action$.pipe(
+//     ofType("TEMP_PING"),
+//     map((action: any) => ({
+//       type: "PUNG",
+//       payload: action.payload * 4
+//     }))
+//   )
+
+const doubleFromSource = (source$) =>
+  source$.pipe(
     map((action: any) => ({
       type: "PONG",
+      payload: action.payload * 2
+    })),
+  )
+const tripleFromSource = (source$) =>
+  source$.pipe(
+    map((action: any) => ({
+      type: "PUNG",
       payload: action.payload * 3
     }))
   )
 
-const secondEpic = (action$) =>
+export const pingEpic = (action$) =>
   action$.pipe(
-    ofType("TEMP_PING"),
-    map((action: any) => ({
-      type: "PUNG",
-      payload: action.payload * 4
-    }))
+    ofType("PING"),
+    mergeMap(action => {
+      const source$ = of(action)
+      return merge(
+        source$.pipe(
+          map((action: any) => ({
+            type: "PONG",
+            payload: action.payload * 2
+          })),
+        ),
+        source$.pipe(
+          map((action: any) => ({
+            type: "PUNG",
+            payload: action.payload * 3
+          }))
+        )
+      )
+    })
   )
 
-export const pingEpic = combineEpics(debug(), seedEpic, firstEpic, secondEpic)
